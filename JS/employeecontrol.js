e2bee8b96e93f8508e6f17bb8e5090eb4871759c
Tcauley91 <caleytyler@gmail.com>
2020-01-12 13:36:35 -0500

    girst node prompt coded

---
+++ JS/employeecontrol.js
@@ -0,0 +1,48 @@
+const inquirer = require("inquirer");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 8080,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "Brandon?!123",
+  database: "employee_trackerDB"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    runSearch();
+  });
+  function runSearch() {
+    inquirer
+      .prompt({
+        name: "action",
+        type: "rawlist",
+        message: "What would you like to do?",
+        choices: [
+          "Add departments, roles, employees",
+          "View departments, roles, employees",
+          "Update employee roles",
+        ]
+      })
+      .then(function(answer) {
+        switch (answer.action) {
+        case "Add departments, roles, employees":
+          addAll();
+          break;
+  
+        case "View departments, roles, employees":
+          viewAll();
+          break;
+  
+        case "Update employee roles":
+          updateEmployee();
+          break;
+        }
+      });
+  }
